c689efc2dedb0b991107e29efdb4d72f5fef2621
z0z1k <37474118+z0z1k@users.noreply.github.com>
2018-03-23 22:10:31 +0200

    magaz

---
+++ pr.php
@@ -0,0 +1,7 @@
+﻿<?
+$zminna1=$_POST['zminna1'];
+$zminna2=$_POST['zminna2'];
+
+$r=$zminna1+$zminna2;
+echo $r;
+?>
---
+++ start.php
@@ -0,0 +1,19 @@
+﻿ <?
+	$_SESSION['golovna']='http://u1.it-elit.org';
+	$_SESSION['name_magazin']='Наш магазин';
+	$host = "itelit.mysql.ukraine.com.ua";
+	$user = "itelit_u1";
+	$password = "123qwerty123";
+	$db="itelit_u1";
+	
+	if (!mysql_connect($host, $user, $password))
+			{
+				echo "<h2>Помилка з*єднення з сервером</h2>";
+				exit;
+			};
+	mysql_select_db($db);
+	
+	mysql_query("SET NAMES 'utf8' COLLATE 'utf8_general_ci'"); 
+	mysql_query("SET CHARACTER SET 'utf8'");
+	
+?>
